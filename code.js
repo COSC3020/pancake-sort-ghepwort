f62537679fd0b8648b164c2b4a049c46a64f8803
ghepwort <ghepwort@uwyo.edu>
2025-02-14 12:49:18 -0700

    Linting fix...?

--- code.js
+++ code.js
@@ -14,6 +14,7 @@ function pancakeSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         let max = arr[0];
         let index = 0;
+
         // Find the max in arr.length - i
         for (var j = 0; j < arr.length - i; j++) {
             if (arr[j] > max) {
@@ -21,10 +22,12 @@ function pancakeSort(arr) {
                 index = j;
             }
         }
+
         // Flip the max to the front
         arr = flip(arr, index);
         // Flip the max to the highest position not taken by the max
         arr = flip(arr, arr.length - 1 - i);
+
     }
     return arr;
 }
